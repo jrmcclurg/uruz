754d7137b1f05cd6826a569f9c7a8bb373874518
Jedidiah McClurg <jrmcclurg@gmail.com>
2012-06-17 12:30:36 -0500

    Forgot to add updated version info

--- version.ml
+++ version.ml
@@ -1,4 +1,4 @@
-let commit = "eacd81106479e3067fc9bbfea7bc65a170731e26";;
-let commit_num = "14";;
-let commit_date = "Sun, 17 Jun 2012 11:24:48 -0500";;
-let build_date = "Sun, 17 Jun 2012 11:25:01 -0500";;
+let commit = "1b9e98116ff4688cb69863738ae250a90df989d4";;
+let commit_num = "15";;
+let commit_date = "Sun, 17 Jun 2012 12:29:48 -0500";;
+let build_date = "Sun, 17 Jun 2012 12:30:03 -0500";;
