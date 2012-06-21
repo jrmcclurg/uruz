95e5d152769fb9641564875b0f9aa795c2c6f873
Jedidiah McClurg <jrmcclurg@gmail.com>
2012-06-21 00:14:42 -0500

    Forgot to update version

--- version.ml
+++ version.ml
@@ -1,4 +1,4 @@
-let commit = "a9e0064510674c9fd6575f790aa2391d32d9233e";;
-let commit_num = "22";;
-let commit_date = "Wed, 20 Jun 2012 17:38:45 -0500";;
-let build_date = "Wed, 20 Jun 2012 22:24:26 -0500";;
+let commit = "a51c11640b5c0d23e22fd96c0e7ec8ea8a263220";;
+let commit_num = "25";;
+let commit_date = "Thu, 21 Jun 2012 00:13:58 -0500";;
+let build_date = "Thu, 21 Jun 2012 00:14:25 -0500";;
