096fe0a3a6dd50dfa0653a491d0a03edee2ccb27
Jedidiah McClurg <jed@jrmcclurg.com>
2014-11-22 16:13:27 -0700

    flattening + property handling

--- bootstrap/main.ml
+++ bootstrap/main.ml
@@ -1,8 +1,15 @@
-open Bootstrap_main;;
-open Flags;;
+open Bootstrap_main
+open Flags
+open Code
 
+let () = (
 let i = parse_command_line () in
+(* parse the input file *)
 let result = get_ast i in
+(* set defaults based on the poperties in input file *)
+handle_props result;
+(* flatten the grammar *)
+let result = flatten_grammar result in
 Bootstrap_ast.print_grammar_t result;
 print_newline();
-exit 0;;
+exit 0)
