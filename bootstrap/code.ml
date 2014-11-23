1c263d83d663745fa137698f81d4e7c06dd1db52
Jedidiah McClurg <jed@jrmcclurg.com>
2014-11-22 18:32:00 -0700

    use symbol table for identifiers

--- bootstrap/code.ml
+++ bootstrap/code.ml
@@ -8,7 +8,7 @@ let rec handle_props (g : grammar_t) : (grammar_t*int) = match g with
 | Grammar(pos,code1,(d,dl),code2) ->
   let (dl2,count) = List.fold_left (fun (acc,index) d -> (match d with
     | PropDecl(p,name,value) -> (
-      match (name,value) with
+      match (get_symbol name,value) with
       | ("default_production_type",StringVal(p,s)) -> Flags.def_prod_type := s (* TODO XXX - this should only be "parser", "ast", etc. *)
       | ("default_production_name",StringVal(p,s)) -> Flags.def_prod_name := s
       | _ -> die_error p "invalid property name or type"
@@ -21,9 +21,9 @@ let rec handle_props (g : grammar_t) : (grammar_t*int) = match g with
 
 (* COLLECT NAMED CODE *)
 
-let collect_named_code (g : grammar_t) (count : int) : (grammar_t * ((string,pos_t*(string option*code) list) Hashtbl.t)) = match g with
+let collect_named_code (g : grammar_t) (count : int) : (grammar_t * ((symb,pos_t*(symb option*code) list) Hashtbl.t)) = match g with
 | Grammar(pos,code1,(d,dl),code2) ->
-  let code_table = (Hashtbl.create count : (string,pos_t*(string option*code) list) Hashtbl.t) in
+  let code_table = (Hashtbl.create count : (symb,pos_t*(symb option*code) list) Hashtbl.t) in
   let dl2 = List.fold_left (fun (acc) d -> (match d with
     | CodeDecl(p,name,(c,cl)) ->
       let l = (c::cl) in
@@ -51,7 +51,7 @@ let (l2,prods,_) = List.fold_left (fun (l2,prods,index) x ->
 ) ([],[], !Flags.def_prod_index) l in
 (List.rev l2, List.rev prods)
 
-let rec flatten_grammar (g : grammar_t) (code_table : (string,pos_t*(string option*code) list) Hashtbl.t) : grammar_t = match g with
+let rec flatten_grammar (g : grammar_t) (code_table : (symb,pos_t*(symb option*code) list) Hashtbl.t) : grammar_t = match g with
 | Grammar(pos,code1,(d,dl),code2) ->
   (*let dl2 = List.rev_map (fun d -> let (x,y) = flatten_decl d in List.rev (x::y)) (d::dl) in
   let l = rev_flatten dl2 in*)
@@ -60,13 +60,13 @@ let rec flatten_grammar (g : grammar_t) (code_table : (string,pos_t*(string opti
   let l = List.rev dl2 in
   Grammar(pos,code1,(List.hd l,List.tl l),code2)
 
-and flatten_decl (d : decl_t) (defname : string option) (deftyp : string option) (nesting : int list) (code_table : (string,pos_t*(string option*code) list) Hashtbl.t) : (decl_t*decl_t list) = match d with
+and flatten_decl (d : decl_t) (defname : symb option) (deftyp : string option) (nesting : int list) (code_table : (symb,pos_t*(symb option*code) list) Hashtbl.t) : (decl_t*decl_t list) = match d with
 | ProdDecl(p,prod) ->
   let (prod2,dl) = flatten_production prod defname deftyp nesting code_table in
   (ProdDecl(p,prod2),dl)
 | _ -> (d,[])
 
-and flatten_production (p : production_t) (defname : string option) (deftyp : string option) (nesting : int list) (code_table : (string,pos_t*(string option*code) list) Hashtbl.t) : (production_t*decl_t list) = match p with
+and flatten_production (p : production_t) (defname : symb option) (deftyp : string option) (nesting : int list) (code_table : (symb,pos_t*(symb option*code) list) Hashtbl.t) : (production_t*decl_t list) = match p with
 | Production(ps,o,pat,patl) ->
   let (defname,deftyp,nesting) = (match o with
     | Some(kwo,(name,ol)) -> (Some(name),kwo,[])
@@ -80,25 +80,25 @@ and flatten_production (p : production_t) (defname : string option) (deftyp : st
   let (patl2,prods) = flatten_list flatten_pattern (pat::patl) defname deftyp nesting code_table in
   (Production(ps,o2,List.hd patl2,List.tl patl2),prods)
 
-and flatten_pattern (p : pattern_t) (defname : string option) (deftyp : string option) (nesting : int list) (code_table : (string,pos_t*(string option*code) list) Hashtbl.t) : (pattern_t*decl_t list) = match p with
+and flatten_pattern (p : pattern_t) (defname : symb option) (deftyp : string option) (nesting : int list) (code_table : (symb,pos_t*(symb option*code) list) Hashtbl.t) : (pattern_t*decl_t list) = match p with
 | Pattern(p,(a,al),eof) ->
   let (al2,prods) = flatten_list flatten_annot_atom (a::al) defname deftyp nesting code_table in
   (Pattern(p,(List.hd al2,List.tl al2),eof),prods)
 
-and flatten_annot_atom (an : annot_atom_t) (defname : string option) (deftyp : string option) (nesting : int list) (code_table : (string,pos_t*(string option*code) list) Hashtbl.t) : (annot_atom_t*decl_t list) = match an with
+and flatten_annot_atom (an : annot_atom_t) (defname : symb option) (deftyp : string option) (nesting : int list) (code_table : (symb,pos_t*(symb option*code) list) Hashtbl.t) : (annot_atom_t*decl_t list) = match an with
 | SingletonAnnotAtom(p,a) -> let (a2,prods) = flatten_atom a defname deftyp nesting code_table in (SingletonAnnotAtom(p,a2),prods)
 | QuantAnnotAtom(p,an,q) -> let (a2,prods) = flatten_annot_atom an defname deftyp nesting code_table in (QuantAnnotAtom(p,a2,q),prods)
 | OptAnnotAtom(p,an,o) ->
   let new_opts = (match o with 
   | CodeNameOption(p,s) ->
-    let (p2,codes) = (try Hashtbl.find code_table s with Not_found -> die_error p ("could not find code declaration named '"^s^"'")) in
+    let (p2,codes) = (try Hashtbl.find code_table s with Not_found -> die_error p ("could not find code declaration named '"^(get_symbol s)^"'")) in
     let o2 = List.rev (List.rev_map (fun (x,y) -> CodeOption(p2,x,y)) codes) in
     o2
   | _ -> [o]) in
   let (a2,prods) = flatten_annot_atom an defname deftyp nesting code_table in
   (List.fold_left (fun acc o -> OptAnnotAtom(p,acc,o)) a2 new_opts,prods)
 
-and flatten_atom (a : atom_t) (defname : string option) (deftyp : string option) (nesting : int list) (code_table : (string,pos_t*(string option*code) list) Hashtbl.t) : (atom_t*decl_t list) = match a with
+and flatten_atom (a : atom_t) (defname : symb option) (deftyp : string option) (nesting : int list) (code_table : (symb,pos_t*(symb option*code) list) Hashtbl.t) : (atom_t*decl_t list) = match a with
 | ProdAtom(p,Production(p2,None,pat,patl)) ->
   let name = Flags.get_def_prod_name defname nesting in
   let (patl2,prods) = flatten_list flatten_pattern (pat::patl) defname deftyp nesting code_table in
@@ -107,6 +107,7 @@ and flatten_atom (a : atom_t) (defname : string option) (deftyp : string option)
   let (patl2,prods) = flatten_list flatten_pattern (pat::patl) (Some(name)) kwo [] code_table in
   (IdentAtom(p,name),(ProdDecl(p2,Production(p2,Some((match kwo with None -> Some(Flags.get_def_prod_type deftyp) | _ -> kwo),(name,ol)),List.hd patl2,List.tl patl2)))::prods)
 | _ -> (a,[])
+
 
 (*********************************************************)
 (**  TOPOLOGICAL SORT FUNCTIONALITY                     **)
