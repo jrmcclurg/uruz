aafc3144d31cdc68ffc2817f5a759fead4d46e1a
Jedidiah McClurg <jrmcclurg@gmail.com>
2014-12-11 18:40:43 -0700

    flatten quantifiers

--- bootstrap/code.ml
+++ bootstrap/code.ml
@@ -55,8 +55,9 @@ let tail_flatten (l : 'a list list) : 'a list =
   List.rev (rev_flatten l)
 
 let flatten_list f l defname deftyp nesting code_table =
+let len = List.length l in
 let (l2,prods,_) = List.fold_left (fun (l2,prods,index) x ->
-  let (x2,prods2) = f x defname deftyp (index::nesting) code_table in
+  let (x2,prods2) = f x defname deftyp (index::nesting) code_table (len=1) in
   ((x2::l2), List.rev_append prods2 prods, index+1)
 ) ([],[], !Flags.def_prod_index) l in
 (List.rev l2, List.rev prods)
@@ -83,17 +84,18 @@ let rec flatten_grammar (g : grammar_t) (code_table : code_hashtbl) : grammar_t 
   (*let dl2 = List.rev_map (fun d -> let (x,y) = flatten_decl d in List.rev (x::y)) (d::dl) in
   let l = rev_flatten dl2 in*)
   let the_list = (d::dl) in
-  let (dl2,_) = List.fold_left (fun (acc,index) d -> let (x,y) = flatten_decl d None None [index] code_table in (List.rev_append (x::y) acc, index+1)) ([],!Flags.def_prod_index) the_list in
+  let b = ((List.length the_list)=1) in
+  let (dl2,_) = List.fold_left (fun (acc,index) d -> let (x,y) = flatten_decl d None None [index] code_table b in (List.rev_append (x::y) acc, index+1)) ([],!Flags.def_prod_index) the_list in
   let l = List.rev dl2 in
   Grammar(pos,(List.hd l,List.tl l))
 
-and flatten_decl (d : decl_t) (defname : symb option) (deftyp : rule_type option) (nesting : int list) (code_table : code_hashtbl) : (decl_t*decl_t list) = match d with
+and flatten_decl (d : decl_t) (defname : symb option) (deftyp : rule_type option) (nesting : int list) (code_table : code_hashtbl) (is_singleton : bool) : (decl_t*decl_t list) = match d with
 | ProdDecl(p,prod) ->
-  let (prod2,dl) = flatten_production prod defname deftyp nesting code_table in
+  let (prod2,dl) = flatten_production prod defname deftyp nesting code_table is_singleton in
   (ProdDecl(p,prod2),dl)
 | _ -> (d,[])
 
-and flatten_production (p : production_t) (defname : symb option) (deftyp : rule_type option) (nesting : int list) (code_table : code_hashtbl) : (production_t*decl_t list) = match p with
+and flatten_production (p : production_t) (defname : symb option) (deftyp : rule_type option) (nesting : int list) (code_table : code_hashtbl) (is_singleton : bool) : (production_t*decl_t list) = match p with
 | Production(ps,o,patl) ->
   let nesting_old = nesting in
   let (defname,deftyp,nesting) = (match o with
@@ -109,15 +111,21 @@ and flatten_production (p : production_t) (defname : symb option) (deftyp : rule
   let (patl2,prods) = flatten_list flatten_pattern patl defname deftyp nesting code_table in
   (Production(ps,o2,patl2),prods)
 
-and flatten_pattern (p : pattern_t) (defname : symb option) (deftyp : rule_type option) (nesting : int list) (code_table : code_hashtbl) : (pattern_t*decl_t list) = match p with
+and flatten_pattern (p : pattern_t) (defname : symb option) (deftyp : rule_type option) (nesting : int list) (code_table : code_hashtbl) (is_singleton : bool) : (pattern_t*decl_t list) = match p with
 | Pattern(p,opts,al) ->
   let opts2 = flatten_opt_list p opts deftyp nesting code_table true in
   let (al2,prods) = flatten_list flatten_annot_atom al defname deftyp nesting code_table in
   (Pattern(p,opts2,al2),prods)
 
-and flatten_annot_atom (an : annot_atom_t) (defname : symb option) (deftyp : rule_type option) (nesting : int list) (code_table : code_hashtbl) : (annot_atom_t*decl_t list) = match an with
-| SingletonAnnotAtom(p,a) -> let (a2,prods) = flatten_atom a defname deftyp nesting code_table in (SingletonAnnotAtom(p,a2),prods)
-| QuantAnnotAtom(p,an,q) -> let (a2,prods) = flatten_annot_atom an defname deftyp nesting code_table in (QuantAnnotAtom(p,a2,q),prods)
+and flatten_annot_atom (an : annot_atom_t) (defname : symb option) (deftyp : rule_type option) (nesting : int list) (code_table : code_hashtbl) (is_singleton : bool) : (annot_atom_t*decl_t list) = match an with
+| SingletonAnnotAtom(p,a) -> let (a2,prods) = flatten_atom a defname deftyp nesting code_table is_singleton in (SingletonAnnotAtom(p,a2),prods)
+| QuantAnnotAtom(p,an,q) ->
+  let (a2,prods) = flatten_annot_atom an defname deftyp (!Flags.def_prod_index::nesting) code_table false in
+  if is_singleton then (QuantAnnotAtom(p,a2,q),prods)
+  else
+    let name = Flags.get_def_prod_name defname nesting in
+    (SingletonAnnotAtom(p,IdentAtom(p,name)),(ProdDecl(p,Production(p,((Some(Flags.get_def_prod_type deftyp)),(Some(name),[])),
+      [Pattern(p,[],[QuantAnnotAtom(p,a2,q)])])))::prods)
 (* TODO XXX - need to flatten these as well *)
 | OptAnnotAtom(p,an,o) ->
   if is_processing_lexer deftyp then
@@ -128,10 +136,10 @@ and flatten_annot_atom (an : annot_atom_t) (defname : symb option) (deftyp : rul
     let o2 = List.rev (List.rev_map (fun (x,y) -> CodeOption(p2,x,y)) codes) in
     o2
   | _ -> [o]) in
-  let (a2,prods) = flatten_annot_atom an defname deftyp nesting code_table in
+  let (a2,prods) = flatten_annot_atom an defname deftyp nesting code_table is_singleton in
   (List.fold_left (fun acc o -> OptAnnotAtom(p,acc,o)) a2 new_opts,prods)
 
-and flatten_atom (a : atom_t) (defname : symb option) (deftyp : rule_type option) (nesting : int list) (code_table : code_hashtbl) : (atom_t*decl_t list) = match a with
+and flatten_atom (a : atom_t) (defname : symb option) (deftyp : rule_type option) (nesting : int list) (code_table : code_hashtbl) (is_singleton : bool) : (atom_t*decl_t list) = match a with
 | IdentAtom(p,_) ->
   if is_processing_lexer deftyp then
     die_error p "lexer productions cannot reference other productions";
