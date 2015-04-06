dbcb3183574d4c236037ca0a0c31b30f2072bc44
Jedidiah McClurg <jed@jrmcclurg.com>
2015-04-06 15:45:42 -0600

    basic framework for code generation

--- bootstrap/flags.ml
+++ bootstrap/flags.ml
@@ -1,15 +1,13 @@
 open Bootstrap_utils
 
 (* flag defaults *)
-let filename = ref "";; (* TODO - this will always be set *)
-let out_file = ref (None : string option)
-
 let only_flatten = ref false
 
 let def_prod_type = ref Parser
 let def_prod_name = ref "Production" (* TODO XXX - make this unique? *)
 let def_prod_index = ref 0
 
+let file_prefix = ref None
 let param_name = ref "x"
 let type_name = ref "t"
 let lexer_prefix = ref "Lex"
@@ -17,8 +15,28 @@ let auto_type_suffix = ref "_t"
 let pos_type_name = ref (add_symbol "pos_t")
 let enable_pos = ref true
 
+let lexer_code = ref (None : (symb option * code) option)
+
 let typecast_table = Hashtbl.create 100
 let def_val_table = Hashtbl.create 10
+
+(* prefix functionality *)
+let get_filename_dir filename = 
+  let dirname = Filename.dirname filename in
+  let result  = (dirname^Filename.dir_sep) in
+  result
+
+let get_filename filename = 
+  let basename = Filename.basename filename in
+  let name     = Filename.chop_extension basename in
+  name
+
+let get_filename_prefix filename = 
+  ((get_filename filename)^"_")
+
+let get_prefix () = match (!filename,!file_prefix) with
+  | (_,Some(pr)) -> pr
+  | (f,_) -> (get_filename f)^"_"
 
 let init_tables () =
 (* default values *)
@@ -85,14 +103,14 @@ let is_processing_lexer (deftyp : rule_type option) = (deftyp=Some(Lexer)) || (d
 let is_processing_parser (deftyp : rule_type option) = (deftyp=Some(Parser)) || (deftyp=None && !def_prod_type=Parser)
 let is_processing_ast (deftyp : rule_type option) = (deftyp=Some(Ast)) || (deftyp=None && !def_prod_type=Ast)
 
-let banner_text = "Welcome to bootstrap v. 1.0";;
+let banner_text = "Welcome to bootstrap v. 2.0";;
 let usage_msg = banner_text^"\n"^
                 "Usage: bootstrap [options] <file>";;
 
 (* parse the command-line arguments *)
 let args = Arg.align [
-   ("-o",        Arg.String(fun x -> out_file := Some(x)),
-                    "<file> Location for the result");
+   (*("-o",        Arg.String(fun x -> out_file := Some(x)),
+                    "<file> Location for the result");*)
 
    ("-flatten",        Arg.Unit(fun () -> only_flatten := true),
                     " Only flatten the grammar and exit");
@@ -110,14 +128,11 @@ let die_system_error (s : string) =
    exit 1
 ;;
 
-let parse_command_line () : in_channel =
-   let f_set = ref false in
-   Arg.parse args (fun x -> f_set := true; filename := x) banner_text;
+let parse_command_line () : string list =
+   let fs = ref ([] : string list) in
+   Arg.parse args (fun x -> fs := x::(!fs)) banner_text;
    (* use the command-line filename if one exists, otherwise use stdin *)
-   match !f_set with
-   | false -> error_usage_msg ()
-   | true -> (
-      try (open_in !filename)
-      with _ -> die_system_error ("can't read from file: "^(!filename))
-   )
+   match (List.rev !fs) with
+   | [] -> error_usage_msg ()
+   | l -> l
 ;;
--- bootstrap/main.ml
+++ bootstrap/main.ml
@@ -11,32 +11,45 @@ let x = Bar(123) in
 match x with
 | (_) -> ()
 ;
-let i = parse_command_line () in
-(* parse the input file *)
-let result = get_ast i in
-Flags.init_tables ();
-(* set defaults based on the poperties in input file *)
-let (result,count) = handle_props result in
-let (result,code_table) = collect_named_code result count in
-let result = inline_grammar result code_table in
-(* flatten the grammar *)
-let result = flatten_grammar result code_table in
-let result = elim_grammar result in (* eliminate quantifiers *)
-Bootstrap_ast.print_grammar_t result;
-if !Flags.only_flatten then (
-  exit 0
-);
-let (result,lexer_prods,lexers) = strip_lexer_grammar result count in
-let result = combine_grammar result lexers in (* combine identical lexers *)
+let fs = parse_command_line () in
+let ret = List.fold_left (fun acc f ->
+  try
+    (* parse the input file *)
+    filename := f;
+    let i = open_in f in
+    let result = get_ast i in
+    let prefix = get_prefix () in
+    Printf.printf "prefix = %s\n" prefix;
+    (*raise (General_error("x"));*)
+    Flags.init_tables ();
+    (* set defaults based on the poperties in input file *)
+    let (result,count) = handle_props result in
+    let (result,code_table) = collect_named_code result count in
+    let result = inline_grammar result code_table in
+    (* flatten the grammar *)
+    let result = flatten_grammar result code_table in
+    let result = elim_grammar result in (* eliminate quantifiers *)
+    Bootstrap_ast.print_grammar_t result;
+    if !Flags.only_flatten then (
+      exit 0
+    );
+    let (result,lexer_prods,lexers) = strip_lexer_grammar result count in
+    let result = combine_grammar result lexers in (* combine identical lexers *)
 
-let (comps,gr) = get_sorted_defs result count in
-Printf.printf "\n\n***********************************\n\n%!";
-Printf.printf "\n\ncomps = %s\n%!" (str_x_list (fun (x,_) -> get_symbol x) comps ", ");
-print_newline();
-Printf.printf "\n\n***********************************\n\n%!";
-let result = typecheck result comps count gr in
-let result = restore_lexer_grammar result lexer_prods in
-Printf.printf "###################################\n";
-Bootstrap_ast.print_grammar_t result;
-Printf.printf "SUCCESS!\n";
-exit 0)
+    let (comps,gr) = get_sorted_defs result count in
+    Printf.printf "\n\n***********************************\n\n%!";
+    Printf.printf "\n\ncomps = %s\n%!" (str_x_list (fun (x,_) -> get_symbol x) comps ", ");
+    print_newline();
+    Printf.printf "\n\n***********************************\n\n%!";
+    let result = typecheck result comps count gr in
+    let result = restore_lexer_grammar result lexer_prods in
+    Printf.printf "###################################\n";
+    Bootstrap_ast.print_grammar_t result;
+    Printf.printf "SUCCESS (%s)!\n" f;
+    output_code prefix result;
+    acc
+  with ex ->
+    Printf.printf "%s\n" (Printexc.to_string ex);
+    acc+1
+) 0 fs in
+exit ret)
